c75463b56e9609d6bc74df758bad4a6be093b338
h8_bugs <cjcyfdek@gmail.com>
2020-10-12 14:11:06 +0300

    created components in all possible ways

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDom from 'react-dom'
 
-function Greeting() {
-  return <h4>Must have!</h4>
+function Welcome(props) {
+  let { name } = props
+
+  return (
+    <React.Fragment>
+      <Intro nickname={name} />
+    </React.Fragment>
+  )
+}
+
+class Intro extends React.Component {
+  render() {
+    return (
+      <section>
+        <div className='centered'>
+          <h2>Nice to meet you, {this.props.nickname}</h2>
+        </div>
+      </section>
+    )
+  }
+}
+
+const Greeting = () => {
+  return React.createElement(Welcome, { name: 'Max;)' })
 }
 
 ReactDom.render(<Greeting />, document.getElementById('root'))
+
 
 
